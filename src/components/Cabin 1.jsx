04a274f2db7462b545b462ab545e42d38cddede3
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-06-21 22:01:32 -0500

    trying to figure out how to pass state from change of select...failing

--- src/components/Cabin 1.jsx
+++ src/components/Cabin 1.jsx
@@ -2,19 +2,25 @@ import { useState } from "react"
 
 
 const Cabin1 = (props) => {
-const [option, setOption] = useState(null)
+    console.log(props.options[1])
+const [option, setOption] = useState("Dirty")
+function handleChange(e) {
+    let value = e.target.value
+    setOption(props.options[value])
+}
 
     return(
         <div>
             <h2>Cabin 1</h2>
             <div>
             <label for ="options">Set Status</label>
-              <select name="options" id="options">
-                <option>Checked Out/Dirty</option>
-                <option>Occupied</option>
-                <option>Clean Immediately</option>
-                <option>Ready</option>
+              <select  >
+                <option value='0'>Checked Out/Dirty</option>
+                <option value='1'>Occupied</option>
+                <option value='2'>Clean Immediately</option>
+                <option value='3'>Ready</option>
               </select>
+              <h3>Status: {option}</h3>
             </div>
         </div>
     )
