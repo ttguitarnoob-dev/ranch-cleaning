04acf916f11a6b3afd08a78a811af123a9219075
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-06-22 08:57:06 -0500

    changing updates status state, but it doesn't persist after reload. Best waya I know how ot do that is with a backend. I'll do that if they wante me to finish it.  It's done for now

--- src/components/Cabin 1.jsx
+++ src/components/Cabin 1.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 const Cabin1 = (props) => {
     console.log(props.options[1])
-const [option, setOption] = useState("Dirty")
+const [option, setOption] = useState("Ready")
+
+// useEffect(() => {
+//     setOption(JSON.parse(window.localStorage.getItem('option')))
+// }, [])
+
+// useEffect(() => {
+//     window.localStorage.setItem('option', option)
+// }, [option])
+
 function handleChange(e) {
     let value = e.target.value
+    console.log(value)
     setOption(props.options[value])
 }
 
@@ -14,7 +24,7 @@ function handleChange(e) {
             <h2>Cabin 1</h2>
             <div>
             <label for ="options">Set Status</label>
-              <select  >
+              <select onChange={handleChange} >
                 <option value='0'>Checked Out/Dirty</option>
                 <option value='1'>Occupied</option>
                 <option value='2'>Clean Immediately</option>
